5c87cb3779666318a4429429d9c040972730779d
varun <varunchaudhary210@gmail.com>
2026-05-05 17:36:28 +0530

    database connection added successfully

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 
 const dbconnection=async()=>{
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/MVC_CRUD")
+        await mongoose.connect("mongodb://127.0.0.1:27017/SCHOOL_DATA")
         console.log("Database is connected............")
     }catch(err){
         console.log("Database connection error",err)
