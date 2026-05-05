22777e340c1ca5c4c6b3f8528a5a4ee0f19e9c6e
varun <varunchaudhary210@gmail.com>
2026-05-05 17:32:11 +0530

    database connection added

---
+++ backend/src/config/db.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose")
+
+const dbconnection=async()=>{
+    try{
+        await mongoose.connect("mongodb://127.0.0.1:27017/MVC_CRUD")
+        console.log("Database is connected............")
+    }catch(err){
+        console.log("Database connection error",err)
+    }
+}
+
+module.exports=dbconnection;
