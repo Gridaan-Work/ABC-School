2332b5a2905cd2f0dd11aff51bac7b6449076242
varun-chaudhary555 <varunchaudhary210@gmail.com>
2026-05-08 17:25:24 +0530

    Update db.js for mongo db atlas

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 
 const dbconnection = async () => {
     try {
-        await mongoose.connect("mongodb://admin:password@localhost:27017/schooldata?authSource=admin")
+        // await mongoose.connect("mongodb://admin:password@localhost:27017/schooldata?authSource=admin")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Database is connected............")
     } catch (err) {
         console.log("Database connection error", err)
